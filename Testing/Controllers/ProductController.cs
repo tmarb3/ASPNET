39245b94a4fc93732a8b7b0621090c4081beaa25
Thomas Marble <tmarble3@Gmail.com>
2022-04-19 14:02:25 -0600

    final commit

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -46,9 +46,26 @@ namespace Testing.Controllers
             return RedirectToAction("ViewProduct", new { id = product.ProductID });
         }
 
+        public IActionResult InsertProduct()
+        {
+            var prod = repo.AssignCategory();
 
+            return View(prod);
+        }
 
+        public IActionResult InsertProductToDatabase(Product productToInsert)
+        {
+            repo.InsertProduct(productToInsert);
 
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+
+            return RedirectToAction("Index");
+        }
 
 
     }
--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -10,8 +10,14 @@ namespace Testing
     {
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
-
         public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+
+        public void DeleteProduct(Product product);
+
+
 
     }
 }
