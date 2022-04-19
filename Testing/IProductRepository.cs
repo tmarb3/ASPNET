32e27dccfed965241c401a07b2d4007ab097de83
Thomas Marble <tmarble3@Gmail.com>
2022-04-19 13:32:12 -0600

    finished view and update product

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+
+        public void UpdateProduct(Product product);
+
+    }
+}
---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("Select * From products");
+        }
+
+
+        public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
+                new { id = id });
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+               new { name = product.Name, price = product.Price, id = product.ProductID });
+
+        }
+    }
+}
